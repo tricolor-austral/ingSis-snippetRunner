6621e303bcac1d2dc22ccc2234ffdb4ff6107d91
SantiGrimoldi <Santiago.grimoldi@ing.austral.edu.ar>
2024-06-21 18:23:07 -0300

    formatting changes

--- src/main/kotlin/ingsis/tircolor/snippetrunner/service/SnippetService.kt
+++ src/main/kotlin/ingsis/tircolor/snippetrunner/service/SnippetService.kt
@@ -1,13 +1,13 @@
-package ingsis.tircolor.SnippetRunner.service
+package ingsis.tircolor.snippetrunner.service
 
+import ingsis.tircolor.SnippetRunner.service.PrintScriptService
 import org.springframework.stereotype.Service
 
 @Service
 class SnippetService {
-
-    fun selectService(language: String): ingsis.tircolor.SnippetRunner.service.Service {
+    fun selectService(language: String): Service {
         return when (language) {
-            "PrintScript" -> PrintScriptService()
+            "PrintScript" -> PrintScriptService() as Service
             else -> throw IllegalArgumentException("Unsupported language: $language")
         }
     }
