149e0329f68b1541a754692dc121c551c718eb33
SantiGrimoldi <Santiago.grimoldi@ing.austral.edu.ar>
2024-06-22 15:43:44 -0300

    Redis works but not async

--- src/main/kotlin/ingsis/tricolor/snippetrunner/controller/RedisController.kt
+++ src/main/kotlin/ingsis/tricolor/snippetrunner/controller/RedisController.kt
@@ -1,24 +1,26 @@
-package ingsis.tricolor.snippetrunner.redis.route
+package ingsis.tricolor.snippetrunner.controller
 
 import ingsis.tricolor.snippetrunner.redis.consumer.FormatProduct
 import ingsis.tricolor.snippetrunner.redis.producer.SnippetFormattedProducer
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RestController("/stream")
-class StreamController
+@RestController
+@RequestMapping("redis")
+class RedisController
     @Autowired
-    constructor(
-        private val producer: SnippetFormattedProducer,
-    ) {
+    constructor(private val producer: SnippetFormattedProducer) {
         @PostMapping("/format/snippet")
         fun createFormatSnippetEvent(
             @RequestBody snippet: FormatProduct,
         ): String {
             println("request has been received")
-            producer.publishEvent(snippet)
+            for (i in 0..2000) {
+                producer.publishEvent(snippet)
+            }
             return "Event to format snippet created"
         }
     }
--- src/main/kotlin/ingsis/tricolor/snippetrunner/service/DefaultRedisService.kt
+++ src/main/kotlin/ingsis/tricolor/snippetrunner/service/DefaultRedisService.kt
@@ -4,27 +4,31 @@ import ingsis.tricolor.snippetrunner.redis.consumer.FormatProduct
 import ingsis.tricolor.snippetrunner.redis.dto.Snippet
 import ingsis.tricolor.snippetrunner.service.interfaces.RedisService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
-import java.io.ByteArrayInputStream
 
 @Service
 class DefaultRedisService
     @Autowired
-    constructor(private val snippetService: PrintScriptService) : RedisService {
-        val operationsApi = WebClient.builder().baseUrl("http://\${permission.url}").build()
+    constructor(
+        private val snippetService: PrintScriptService,
+        @Value("\${permission.url}") private val permissionUrl: String,
+    ) : RedisService {
+        val operationsApi = WebClient.builder().baseUrl("http://$permissionUrl").build()
 
         override fun formatSnippet(snippet: FormatProduct): Snippet {
             // TODO: tengo que formatear el snippet y mandarselo a el operations (snippetholder) para que lo guarde
-            val stream = ByteArrayInputStream(snippet.snippet.toByteArray(Charsets.UTF_8))
-            val formattedSnippet = snippetService.format(stream, "1.1", "Aca le pasaria las reglas en ves del path")
-            val bodyValue = Snippet(snippet.id, formattedSnippet.toString())
-            operationsApi.post()
-                .uri("/update-snippet-runner")
-                .bodyValue(bodyValue)
-                .retrieve()
-                .bodyToMono(String::class.java)
-                .block() ?: throw RuntimeException("Couldn't save snippet")
+//            val stream = ByteArrayInputStream(snippet.snippet.toByteArray(Charsets.UTF_8))
+//            val formattedSnippet = snippetService.format(stream, "1.1", "Aca le pasaria las reglas en ves del path")
+//            val bodyValue = Snippet(snippet.id, formattedSnippet.toString())
+            val bodyValue = Snippet(snippet.id, snippet.snippet)
+//            operationsApi.post()
+//                .uri("/update-snippet-runner")
+//                .bodyValue(bodyValue)
+//                .retrieve()
+//                .bodyToMono(String::class.java)
+//                .block() ?: throw RuntimeException("Couldn't save snippet")
 
             return bodyValue
         }
